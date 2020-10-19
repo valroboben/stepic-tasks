7211222cd753ec580aeb38eb3bbda523d251e2c2
Val Roboben <valroboben@yandex.ru>
2020-10-19 14:06:41 +0300

    code corrected

--- live_dead.cs
+++ live_dead.cs
@@ -11,10 +11,22 @@ namespace livedead
 
         static int findDaysInMonth(int i, int daysInMonth, int maybeVisokosnyGod)
         {
-            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) { daysInMonth = 31; }
-            if (i == 4 || i == 6 || i == 9 || i == 11) { daysInMonth = 30;  }
-            if (i == 2 && maybeVisokosnyGod % 4 != 0) { daysInMonth = 28; }
-            if (i == 2 && maybeVisokosnyGod % 4 == 0) { daysInMonth = 29; }
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
+            {
+                daysInMonth = 31;
+            }
+            if (i == 4 || i == 6 || i == 9 || i == 11)
+            {
+                daysInMonth = 30;
+            }
+            if (i == 2 && maybeVisokosnyGod % 4 != 0)
+            {
+                daysInMonth = 28;
+            }
+            if (i == 2 && maybeVisokosnyGod % 4 == 0)
+            {
+                daysInMonth = 29;
+            }
             return daysInMonth;
         }
 
@@ -22,7 +34,11 @@ namespace livedead
         {
             bool caseSwitchCheck = false;
             int caseSwitchCheckLength = caseSwitchInput.Length;
-            if (caseSwitchCheckLength > 1) { Console.WriteLine("Your input is too long, start again");  caseSwitchCheck = true; }
+            if (caseSwitchCheckLength > 1)
+            {
+                Console.WriteLine("Your input is too long, start again");
+                caseSwitchCheck = true;
+            }
 
             char[] caseSwitchInputArray = caseSwitchInput.ToCharArray();
             for (int i = 0; i < caseSwitchCheckLength; i++)
@@ -42,7 +58,12 @@ namespace livedead
             bool dateInputCheck = false;
             int counter = 0;
             int someDateStringLength = someDateString.Length;
-            if (someDateStringLength < 5) { Console.WriteLine("The date you have inpus is TOO short, please, try again!"); dateInputCheck = true; goto EndDateInputCheck; }
+            if (someDateStringLength < 5)
+            {
+                Console.WriteLine("The date you have inpus is TOO short, please, try again!");
+                dateInputCheck = true;
+                goto EndDateInputCheck;
+            }
 
             char[] someDateCharArray = someDateString.ToCharArray();
             for (int i = 0; i < someDateStringLength; i++)
@@ -62,7 +83,10 @@ namespace livedead
                     Console.WriteLine("There mustn't be to DOTS in a row!");
                     dateInputCheck = true; goto EndDateInputCheck;
                 }
-                if ((int)someDateCharArray[i] == 46) { counter++; }
+                if ((int)someDateCharArray[i] == 46)
+                {
+                    counter++;
+                }
             }
             if (counter != 2)
             {
@@ -81,7 +105,8 @@ namespace livedead
             {
                 int number = Convert.ToInt32(someDateDDMMYYYYArray[i]);
                 someDateDDMMYYYYNumericArray[i] = number;
-            } // created dd.mm.yyyy array for input date
+            } 
+            // created dd.mm.yyyy array for input date
 
             DateTime dt = DateTime.Now;
             string currentDateDDMMYYYCheck = dt.ToShortDateString();
@@ -91,7 +116,8 @@ namespace livedead
             {
                 int number = Convert.ToInt32(currentDateDDMMYYYYArray[i]);
                 currentDateDDMMYYYYNumericArray[i] = number;
-            } // created dd.mm.yyyy array for current date
+            } 
+            // created dd.mm.yyyy array for current date
 
             if (currentDateDDMMYYYYNumericArray[2] < someDateDDMMYYYYNumericArray[2])
             {
@@ -121,7 +147,8 @@ namespace livedead
             }
         EndDateInputCheck:
             return dateInputCheck;
-        } // we've checked the input format dd.mm.yyyy
+        } 
+        // we've checked the input format dd.mm.yyyy
 
 
         static int yearsFull(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast, int daysOfLife, int peremennaya)
@@ -132,9 +159,10 @@ namespace livedead
                 daysOfLife += 365;
             }
             return daysOfLife;
-        }  // counts FULL YEARS that were lived
+        }
+        // counts FULL YEARS that were lived
 
-    static int monthFullFirstOrOnly(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast, int daysOfLife, int peremennaya)
+        static int monthFullFirstOrOnly(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast, int daysOfLife, int peremennaya)
         {
             if (yyyyBirth != yyyyLast)
             {
@@ -155,22 +183,39 @@ namespace livedead
                 }
             }
             return daysOfLife;
-        }  // counts days of FULL MONTHS of the 1-st or THE ONLY life year
+        }  
+        // counts days of FULL MONTHS of the 1-st or THE ONLY life year
 
         static int daysFirstOrOnlyMonth(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast, int daysOfLife, int peremennaya)
         {
-            if (mmBirth == 1 || mmBirth == 3 || mmBirth == 5 || mmBirth == 7 || mmBirth == 8 || mmBirth == 10 || mmBirth == 12) { peremennaya = 31; }
-            if (mmBirth == 4 || mmBirth == 6 || mmBirth == 9 || mmBirth == 11) { peremennaya = 30; }
-            if (mmBirth == 2) { peremennaya = 28; }
-            if (mmBirth == 2 && yyyyBirth % 4 == 0) { peremennaya = 29; }
-            if (mmBirth == mmLast && yyyyBirth == yyyyLast) { peremennaya = ddLast; }
+            if (mmBirth == 1 || mmBirth == 3 || mmBirth == 5 || mmBirth == 7 || mmBirth == 8 || mmBirth == 10 || mmBirth == 12)
+            {
+                peremennaya = 31;
+            }
+            if (mmBirth == 4 || mmBirth == 6 || mmBirth == 9 || mmBirth == 11)
+            {
+                peremennaya = 30;
+            }
+            if (mmBirth == 2)
+            {
+                peremennaya = 28;
+            }
+            if (mmBirth == 2 && yyyyBirth % 4 == 0)
+            {
+                peremennaya = 29;
+            }
+            if (mmBirth == mmLast && yyyyBirth == yyyyLast)
+            {
+                peremennaya = ddLast;
+            }
             for (int i = ddBirth; i <= peremennaya; i++)
             {
                 daysOfLife++;
              //   Console.WriteLine("plus day first month!");
             }
             return daysOfLife; 
-        }  // counts days of the first NOT FULL month in life OR of the ONLY MONTH in one's life
+        }  
+        // counts days of the first NOT FULL month in life OR of the ONLY MONTH in one's life
 
         static int monthFullLastYear(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast, int daysOfLife, int peremennaya)
         {
@@ -184,29 +229,46 @@ namespace livedead
                 }
             }
             return daysOfLife;
-        }  // counts FULL MONTHS of the LAST YEAR (if it is NOT THE ONLY YEAR in life)
+        }  
+        // counts FULL MONTHS of the LAST YEAR (if it is NOT THE ONLY YEAR in life)
 
         static int daysLastMonthLastYear(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast, int daysOfLife, int peremennaya)
         {
             {
                 for (int i = 1; i <= ddLast; i++)
-                    if (yyyyBirth == yyyyLast && mmBirth == mmLast) { break; }// не входит в цикл при значении годарождения 2020 + не входит в цикл при значении месяца ТЕКУЩИЙ (10)
+                    if (yyyyBirth == yyyyLast && mmBirth == mmLast)
+                    {
+                        break;
+                    }
                     else
-                   { daysOfLife++; }
+                    {
+                        daysOfLife++;
+                    }
             }
             return daysOfLife;
-        }  // counts days of the last NOT FULL month (if it is NOT THE ONLY in life)
+        }  
+        // counts days of the last NOT FULL month (if it is NOT THE ONLY in life)
 
         static bool birthAndDeathCheck(int ddBirth, int mmBirth, int yyyyBirth, int ddLast, int mmLast, int yyyyLast)
         {
             bool deadBeforeBirth = false;
             if (yyyyLast < yyyyBirth)
-            { Console.WriteLine("death YEAR is earlier than birth year? input death date again!"); deadBeforeBirth = true; goto EndBirthAndDeathCheck;  }
+            {
+                Console.WriteLine("death YEAR is earlier than birth year? input death date again!");
+                deadBeforeBirth = true;
+                goto EndBirthAndDeathCheck;
+            }
             if (yyyyLast == yyyyBirth && mmLast < mmBirth)
-            { Console.WriteLine("death MONTH is earlier than birth month? input death date again!"); deadBeforeBirth = true; goto EndBirthAndDeathCheck; }
+            {
+                Console.WriteLine("death MONTH is earlier than birth month? input death date again!");
+                deadBeforeBirth = true;
+                goto EndBirthAndDeathCheck;
+            }
             if (yyyyLast == yyyyBirth && mmLast == mmBirth && ddLast < ddBirth)
-            { Console.WriteLine("death DAY is earlier than birth day? input death date again!"); deadBeforeBirth = true; }
-
+            {
+                Console.WriteLine("death DAY is earlier than birth day? input death date again!");
+                deadBeforeBirth = true;
+            }
         EndBirthAndDeathCheck:
             return deadBeforeBirth;
         }
@@ -224,10 +286,16 @@ namespace livedead
             Console.Write("If you want to count life days of a dead person, input 2 "); Console.WriteLine();
             string caseSwitchInput = Console.ReadLine();
             if (String.IsNullOrWhiteSpace(caseSwitchInput))
-            { Console.WriteLine("You have input an empty line. Let's start again from the very beginning!"); goto Begin; }
+            {
+                Console.WriteLine("You have input an empty line. Let's start again from the very beginning!");
+                goto Begin;
+            }
 
             thereIsAnError = caseSwitchCheck(caseSwitchInput);
-            if (thereIsAnError == true) { goto Begin; }
+            if (thereIsAnError == true)
+            {
+                goto Begin;
+            }
 
             int caseSwitchInputNumber = Convert.ToInt32(caseSwitchInput);
 
@@ -238,10 +306,16 @@ namespace livedead
                     Console.Write("Input birth date in format dd.mm.yyyy ");
                     string birthDateString = Console.ReadLine();
                     if (String.IsNullOrWhiteSpace(birthDateString))
-                    { Console.WriteLine("You have input an empty line. Input birth date again!"); goto case 1; }
+                    {
+                        Console.WriteLine("You have input an empty line. Input birth date again!");
+                        goto case 1;
+                    }
 
                     thereIsAnError = dateInputCheck(birthDateString);
-                    if (thereIsAnError == true) { goto case 1; }
+                    if (thereIsAnError == true)
+                    {
+                        goto case 1;
+                    }
 
                     string[] birthDateDDMMYYYYArray = birthDateString.Split('.');
                     int[] birthDateDDMMYYYYNumericArray = new int[3];
@@ -249,7 +323,8 @@ namespace livedead
                     {
                         int number = Convert.ToInt32(birthDateDDMMYYYYArray[i]);
                         birthDateDDMMYYYYNumericArray[i] = number;
-                    } // created dd.mm.yyyy array for input date
+                    } 
+                    // created dd.mm.yyyy array for input date
 
                     DateTime dt = DateTime.Now;
                     string currentDateDDMMYYYCheck = dt.ToShortDateString();
@@ -259,7 +334,8 @@ namespace livedead
                     {
                         int number = Convert.ToInt32(currentDateDDMMYYYYArray[i]);
                         currentDateDDMMYYYYNumericArray[i] = number;
-                    } // created dd.mm.yyyy array for current date
+                    } 
+                    // created dd.mm.yyyy array for current date
 
                     daysOfLife = yearsFull(birthDateDDMMYYYYNumericArray[0], birthDateDDMMYYYYNumericArray[1], birthDateDDMMYYYYNumericArray[2], currentDateDDMMYYYYNumericArray[0], currentDateDDMMYYYYNumericArray[1], currentDateDDMMYYYYNumericArray[2], daysOfLife, 0);
                     daysOfLife = monthFullFirstOrOnly(birthDateDDMMYYYYNumericArray[0], birthDateDDMMYYYYNumericArray[1], birthDateDDMMYYYYNumericArray[2], currentDateDDMMYYYYNumericArray[0], currentDateDDMMYYYYNumericArray[1], currentDateDDMMYYYYNumericArray[2], daysOfLife, 0); // посчитали прожитые ПОЛНЫЕ месяцы ПЕРВОГО или ЕДИНСТВЕННОГО года
@@ -274,10 +350,16 @@ namespace livedead
                     Console.Write("Input birth date in format dd.mm.yyyy ");
                     birthDateString = Console.ReadLine();
                     if (String.IsNullOrWhiteSpace(birthDateString))
-                    { Console.WriteLine("You have input an empty line. Input birth date again!"); goto case 2; }
+                    {
+                        Console.WriteLine("You have input an empty line. Input birth date again!");
+                        goto case 2;
+                    }
 
                     thereIsAnError = dateInputCheck(birthDateString);
-                    if (thereIsAnError == true) { goto case 2; }
+                    if (thereIsAnError == true)
+                    {
+                        goto case 2;
+                    }
 
                     birthDateDDMMYYYYArray = birthDateString.Split('.');
                     birthDateDDMMYYYYNumericArray = new int[3];
@@ -285,16 +367,23 @@ namespace livedead
                     {
                         int number = Convert.ToInt32(birthDateDDMMYYYYArray[i]);
                         birthDateDDMMYYYYNumericArray[i] = number;
-                    } // created dd.mm.yyyy array for input date
+                    }
+                    // created dd.mm.yyyy array for input date
 
                     InputDeathDate:
                     Console.Write("Input death date in format dd.mm.yyyy ");
                     string deathDateString = Console.ReadLine();
                     if (String.IsNullOrWhiteSpace(deathDateString))
-                    { Console.WriteLine("You have input an empty line. Input death date again!"); goto InputDeathDate; }
+                    {
+                        Console.WriteLine("You have input an empty line. Input death date again!");
+                        goto InputDeathDate;
+                    }
 
                     thereIsAnError = dateInputCheck(deathDateString);
-                    if (thereIsAnError == true) { goto InputDeathDate; }
+                    if (thereIsAnError == true)
+                    {
+                        goto InputDeathDate;
+                    }
 
                     string[] deathDateDDMMYYYYArray = deathDateString.Split('.');
                     int[] deathDateDDMMYYYYNumericArray = new int[3];
@@ -302,10 +391,14 @@ namespace livedead
                     {
                         int number = Convert.ToInt32(deathDateDDMMYYYYArray[i]);
                         deathDateDDMMYYYYNumericArray[i] = number;
-                    } // created dd.mm.yyyy array for input death date
+                    } 
+                    // created dd.mm.yyyy array for input death date
 
                     bool deadBeforeBirth = birthAndDeathCheck(birthDateDDMMYYYYNumericArray[0], birthDateDDMMYYYYNumericArray[1], birthDateDDMMYYYYNumericArray[2], deathDateDDMMYYYYNumericArray[0], deathDateDDMMYYYYNumericArray[1], deathDateDDMMYYYYNumericArray[2]);
-                    if (deadBeforeBirth == true) { goto InputDeathDate; }
+                    if (deadBeforeBirth == true)
+                    {
+                        goto InputDeathDate;
+                    }
 
                     daysOfLife = yearsFull(birthDateDDMMYYYYNumericArray[0], birthDateDDMMYYYYNumericArray[1], birthDateDDMMYYYYNumericArray[2], deathDateDDMMYYYYNumericArray[0], deathDateDDMMYYYYNumericArray[1], deathDateDDMMYYYYNumericArray[2], daysOfLife, 0);
                     daysOfLife = monthFullFirstOrOnly(birthDateDDMMYYYYNumericArray[0], birthDateDDMMYYYYNumericArray[1], birthDateDDMMYYYYNumericArray[2], deathDateDDMMYYYYNumericArray[0], deathDateDDMMYYYYNumericArray[1], deathDateDDMMYYYYNumericArray[2], daysOfLife, 0); // посчитали прожитые ПОЛНЫЕ месяцы ПЕРВОГО или ЕДИНСТВЕННОГО года
@@ -321,7 +414,10 @@ namespace livedead
             Console.WriteLine("want to try again? input 8 an press enter!");
             Console.WriteLine("If you want the programm to be closed - input anything but 8.");
             string input = Console.ReadLine();
-            if (input == "8") { goto Begin; }
+            if (input == "8")
+            {
+                goto Begin;
+            }
             else Console.WriteLine("have a good day, bye!");
 
 
